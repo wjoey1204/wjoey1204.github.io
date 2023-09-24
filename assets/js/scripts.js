f962d7b43896d9572b6d4f8c8d201501ca699cac
wjoey1204 <126858673+wjoey1204@users.noreply.github.com>
2023-09-24 14:28:12 +0800

    add serif theme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var body = document.querySelector('body')
+var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
+var menuContainer = document.querySelector('#main-menu-mobile');
 
+menuTrigger.onclick = function() {
+    menuContainer.classList.toggle('open');
+    menuTrigger.classList.toggle('is-active')
+    body.classList.toggle('lock-scroll')
+}
